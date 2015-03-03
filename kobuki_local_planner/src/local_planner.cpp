af0955677127b1d292b7903ed680dd5c972a3bce
Daiki Maekawa <d.maekawa@rapyuta-robotics.com>
2015-03-03 16:55:55 +0900

    Fix access path of trajectory msg

--- kobuki_local_planner/src/local_planner.cpp
+++ kobuki_local_planner/src/local_planner.cpp
@@ -10,14 +10,27 @@ public:
     }
 
     void spin(){
-        ros::spin();
+        ros::Rate r(10);
+
+        while(ros::ok()){
+            double min_goal_dist = 99999999;
+            for(int i=0; i < _path_msg_gl.result.planned_trajectory.multi_dof_joint_trajectory.points.size(); i++){
+                double x = _path_msg_gl.result.planned_trajectory.multi_dof_joint_trajectory.points[i].transforms[0].translation.x;
+                double y = _path_msg_gl.result.planned_trajectory.multi_dof_joint_trajectory.points[i].transforms[1].translation.y;
+                double z = _path_msg_gl.result.planned_trajectory.multi_dof_joint_trajectory.points[i].transforms[2].translation.z;
+                ROS_INFO_STREAM("path[" << i << "] = (" << x << ", " << y << ", " << z << ")");
+            }
+
+            r.sleep();
+            ros::spinOnce();
+        }
     }
 
 private:
     void global_path_callback(const moveit_msgs::MoveGroupActionResult &path_msg){
         _path_msg_gl = path_msg;
         ROS_INFO_STREAM("path = " << _path_msg_gl);
-
+        //ROS_INFO_STREAM("joint_names = " << _path_msg_gl.result.planned_trajectory.multi_dof_joint_trajectory.joint_names[0]);
     }
     
     ros::Subscriber _path_sub;
