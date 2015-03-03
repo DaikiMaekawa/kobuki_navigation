1464ab0d73e9951bc216e9df37bc69d5a2071a8d
Daiki Maekawa <d.maekawa@rapyuta-robotics.com>
2015-03-03 16:19:31 +0900

    Add source file

---
+++ kobuki_local_planner/src/local_planner.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <moveit_msgs/MoveGroupActionResult.h>
+
+class LocalPlanner{
+public: 
+    LocalPlanner(ros::NodeHandle &node) : 
+        _path_sub(node.subscribe("/move_group/result", 10, &LocalPlanner::global_path_callback, this))
+    {
+
+    }
+
+    void spin(){
+        ros::spin():
+    }
+
+private:
+    void global_path_callback(const moveit_msgs::MoveGroupActionResult &path_msg){
+        _path_msg_gl = path_msg;
+        ROS_INFO_STREAM("path = " << _path_msg_gl);
+
+    }
+    
+    ros::Subscriber _path_sub;
+    moveit_msgs::MoveGroupActionResult _path_msg_gl;
+
+};
+
+int main(int argc, char *argv[]){
+    ros::init(argc, argv, "local_planner");
+
+    ros::NodeHandle node;
+    LocalPlanner lp(node);
+    lp.spin();
+
+    return 0;
+}
+
