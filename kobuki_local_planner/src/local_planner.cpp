3d1d68120c5a15ebd42760f5c48dec2f532ecbe0
Daiki Maekawa <d.maekawa@rapyuta-robotics.com>
2015-03-04 16:37:36 +0900

    Publish twist message

--- kobuki_local_planner/src/local_planner.cpp
+++ kobuki_local_planner/src/local_planner.cpp
@@ -3,15 +3,49 @@
 #include <mocap_msgs/MoCapPose.h>
 #include <trajectory_msgs/MultiDOFJointTrajectory.h>
 #include <trajectory_msgs/MultiDOFJointTrajectoryPoint.h>
+#include <geometry_msgs/Twist.h>
 
 #include <vector>
 #include <math.h>
+
+double distance_points(double p1[3], double p2[3]){
+    return sqrt(pow(p1[0]-p2[0], 2) + pow(p1[1]-p2[1], 2));
+}
+
+double direction_deg(double p_lc[3]) {
+    return atan2(p_lc[1], p_lc[0]) * 180.0 / 3.1415;
+}
+
+void rotation2d(double p_in[3], double dq, double p_out[3]){
+    p_out[0] = cos(dq) * p_in[0] - sin(dq) * p_in[1];
+    p_out[1] = sin(dq) * p_in[0] + cos(dq) * p_in[1];
+    p_out[2] = p_in[2] + dq;
+}
+
+double coord_trans_global_to_local(double p1_gl[3], double p2_gl[3], double p_out[3]){
+    double p12[3];
+    p12[0] = p2_gl[0]-p1_gl[0];
+    p12[1] = p2_gl[1]-p1_gl[1];
+    p12[2] = p2_gl[2]-p1_gl[2];
+    
+    rotation2d(p12, -p1_gl[2], p_out);
+}
+
+double clamp(double value, double high, double low){
+    if(value > high) {
+        return high;
+    } else {
+        if(value < low) return low;
+        else            return value;
+    }
+}
 
 class LocalPlanner{
 public: 
     LocalPlanner(ros::NodeHandle &node) : 
         _path_sub(node.subscribe("/move_group/result", 10, &LocalPlanner::global_path_callback, this)),
-        _pose_sub(node.subscribe("/kobuki/mocap_pose", 10, &LocalPlanner::self_pose_callback, this))
+        _pose_sub(node.subscribe("/kobuki/mocap_pose", 10, &LocalPlanner::self_pose_callback, this)),
+        _cmd_pub(node.advertise<geometry_msgs::Twist>("cmd_vel", 10))
     {
         _path_gl_it = _path_gl.points.begin();
     }
@@ -20,26 +54,40 @@ public:
         ros::Rate r(10);
 
         while(ros::ok()){
+            geometry_msgs::Twist cmd;
+
             if(_path_gl_it != _path_gl.points.end()){
-                double t_pos[3];
-                t_pos[0] = _path_gl_it->transforms[0].translation.x;
-                t_pos[1] = _path_gl_it->transforms[0].translation.y;
-                t_pos[2] = _path_gl_it->transforms[0].translation.z;
+                double t_pos_gl[3];
+                t_pos_gl[0] = _path_gl_it->transforms[0].translation.x;
+                t_pos_gl[1] = _path_gl_it->transforms[0].translation.y;
+                t_pos_gl[2] = 0;
                 
                 double self_pos[3];
                 self_pos[0] = _self_pose.pose.position.x;
                 self_pos[1] = _self_pose.pose.position.y;
-                self_pos[2] = _self_pose.pose.position.z;
+                self_pos[2] = 0;
 
-                double dist = distance_points(t_pos, self_pos);
-                ROS_INFO_STREAM("target_pos = (" << t_pos[0] << ", " << t_pos[1] << ", " << t_pos[2] << ")");
+                double dist = distance_points(t_pos_gl, self_pos);
+                ROS_INFO_STREAM("target_pos = (" << t_pos_gl[0] << ", " << t_pos_gl[1] << ", " << t_pos_gl[2] << ")");
                 ROS_INFO_STREAM("self_pos = (" << self_pos[0] << ", " << self_pos[1] << ", " << self_pos[2] << ")");
                 ROS_INFO_STREAM("dist = " << dist);
+                
+                double t_pos_lc[3];
+                coord_trans_global_to_local(self_pos, t_pos_gl, t_pos_lc);
+                double dir = direction_deg(t_pos_lc);
+                ROS_INFO_STREAM("dir = " << dir);
 
-                if(dist < 0.5) {
+                cmd.linear.x = clamp(t_pos_lc[0], 1.0, -1.0);
+                cmd.linear.y = clamp(t_pos_lc[1], 1.0, -1.0);
+                //cmd.angular.z = clamp(dir * 0.01, 1.0, -1.0);
+
+                if(dist < 0.3) {
                     _path_gl_it++;
                 }
             }
+
+            ROS_INFO("loop");
+            _cmd_pub.publish(cmd);
 
             r.sleep();
             ros::spinOnce();
@@ -47,10 +95,7 @@ public:
     }
 
 private:
-    double distance_points(double p1[3], double p2[3]){
-        return sqrt(pow(p1[0]-p2[0], 2) + pow(p1[1]-p2[1], 2) + pow(p1[2]-p2[2], 2));
-    }
-
+    
     void self_pose_callback(const mocap_msgs::MoCapPose &pose_msg){
         _self_pose = pose_msg;
         //ROS_INFO_STREAM("pose = " << _self_pose);
@@ -63,6 +108,7 @@ private:
     }
     
     ros::Subscriber _path_sub, _pose_sub;
+    ros::Publisher _cmd_pub;
     //moveit_msgs::MoveGroupActionResult _path_msg_gl;
     mocap_msgs::MoCapPose _self_pose;
     trajectory_msgs::MultiDOFJointTrajectory _path_gl;
